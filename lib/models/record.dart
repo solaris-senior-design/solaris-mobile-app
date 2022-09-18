4a209b24a8cc6d93dfc18123d3d81be9d16c59df
Jaime Garcia, Jr <40704782+JJgar2725@users.noreply.github.com>
2022-09-18 13:10:56 -0500

    removed metric model class to remove redundancy

--- lib/models/record.dart
+++ lib/models/record.dart
@@ -1,33 +1,58 @@
-import 'package:solaris_mobile_app/models/metrics_model.dart';
+import 'temperature.dart';
+import 'voltage.dart';
+import 'battery_capacity.dart';
+import 'current.dart';
+import 'irradiance.dart';
 import 'metric.dart';
 
 class Record {
   final DateTime _createdAt;
-  final MetricsModel _metricsModel;
+  final Voltage _voltage;
+  final Current _current;
+  final Temperature _temperature;
+  final Irradiance _irradiance;
+  final BatteryCapacity _batteryCapacity;
 
-  Record(this._createdAt, this._metricsModel);
+  Record(this._createdAt, this._voltage, this._current, this._temperature,
+      this._irradiance, this._batteryCapacity);
 
   factory Record.fromJson(Map<String, dynamic> json) {
     DateTime time = DateTime.parse(json["createdAt"]).toLocal();
-    MetricsModel metricsModel = MetricsModel.fromJson(json);
+    Voltage voltage = Voltage.fromJson(json);
+    Current current = Current.fromJson(json);
+    Temperature temperature = Temperature.fromJson(json);
+    Irradiance irradiance = Irradiance.fromJson(json);
+    BatteryCapacity batteryCapacity = BatteryCapacity.fromJson(json);
 
-    return Record(time, metricsModel);
+    return Record(
+        time, voltage, current, temperature, irradiance, batteryCapacity);
   }
 
   DateTime get createdAt {
     return _createdAt;
   }
 
-  MetricsModel get metricsModel {
-    return _metricsModel;
+  Voltage get voltage {
+    return _voltage;
+  }
+
+  Current get current {
+    return _current;
+  }
+
+  Temperature get temperature {
+    return _temperature;
+  }
+
+  Irradiance get irradiance {
+    return _irradiance;
+  }
+
+  BatteryCapacity get batteryCapacity {
+    return _batteryCapacity;
   }
 
   List<Metric> getDataMetrics() {
-    return [
-      metricsModel.voltage,
-      metricsModel.current,
-      metricsModel.temperature,
-      metricsModel.batteryCapacity
-    ];
+    return [_voltage, _current, _temperature, _batteryCapacity];
   }
 }
