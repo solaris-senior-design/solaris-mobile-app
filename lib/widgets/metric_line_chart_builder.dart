e983c46147591ea325938fdb30f1e3484d6d0889
Jaime Garcia, Jr <40704782+JJgar2725@users.noreply.github.com>
2022-10-04 15:29:06 -0500

    removed unused import

--- lib/widgets/metric_line_chart_builder.dart
+++ lib/widgets/metric_line_chart_builder.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:solaris_mobile_app/widgets/power_bar_chart_card.dart';
 
 import '../models/metric_line_chart.dart';
 import '../utils/constants.dart';
